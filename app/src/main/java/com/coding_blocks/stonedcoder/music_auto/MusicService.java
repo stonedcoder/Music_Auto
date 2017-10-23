4a0c9846a1700459d7f188c4532c828007ed22f3
stonedcoder <vraunaq1213@gmail.com>
2017-10-23 12:42:47 +0530

    Initial commit

---
+++ app/src/main/java/com/coding_blocks/stonedcoder/music_auto/MusicService.java
@@ -0,0 +1,13 @@
+package com.coding_blocks.stonedcoder.music_auto;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+
+/**
+ * Created by stonedcoder on 23/10/17.
+ */
+
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+public class MusicService {
+    
+}
